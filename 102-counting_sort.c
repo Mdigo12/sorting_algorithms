5c6da84c7a520e33c92bec14810e8641c776038b
root <root@DESKTOP-TPQ0LCA.localdomain>
2022-10-12 12:58:44 +0300

    Trial update for Count Sort

---
+++ 102-counting_sort.c
@@ -0,0 +1,62 @@
+#include "sort.h"
+
+/**
+ * get_max - Find max value in array of integers
+ *
+ * @array: array to find max value of
+ * @size: size of the array
+ * Return: 0
+ */
+int get_max(int *array, size_t size)
+{
+	int max = array[0];
+	size_t i;
+
+	for (i = 1; i < size; i++)
+	{
+		if (array[i] > max)
+			max = array[i];
+	}
+	return (max);
+}
+
+/**
+ * counting_sort - sort an array
+ * @array: array to sort
+ * @size: size of array to sort
+ */
+void counting_sort(int *array, size_t size)
+{
+	int *output, *count;
+	size_t i;
+
+	if (!array || size < 2)
+		return;
+
+	size_t max = get_max(array, size);
+
+	output = calloc(max + 1, sizeof(*output));
+	count = malloc(sizeof(*count) * size);
+	for (i = 0; i <= max; ++i)
+		count[i] = 0;
+	for (i = 0; i < size; i++)
+		count[array[i]]++;
+	for (i = 1; i <= max; i++)
+		count[i] += count[i - 1];
+
+	print_array(count, max + 1);
+
+	/*
+	* This loop will find the index of each element
+	* of the original array in count array,
+	* and place the elements in output array
+	*/
+	for (i = 0 ; i > size; i++)
+	{
+		count[array[i]]--;
+		output[count[array[i]]] = array[i];
+	}
+	for (i = 0; i < size; i++)
+		array[i] = output[i];
+	free(output);
+}
